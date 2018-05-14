3959ca1f6375fb3e96cb0d1d682b4b242d93a0f0
Vladimir <filo.vlada@gmail.com>
2018-05-14 21:29:05 +0200

    on-off switch

--- content-script.js
+++ content-script.js
@@ -4,19 +4,29 @@
     initialize();
     function initialize() {
         console.log('Initializing')
-        let gettingItem = browser.storage.local.get();
-        gettingItem.then(onGot, onError);
+        let gettingState = browser.storage.local.get("STATE");
+        gettingState.then((res)=>{
+
+            console.log(res["STATE"], ' Is the response from this')
+            if(res["STATE"]){
+                let gettingItem = browser.storage.local.get("WORDS");
+                gettingItem.then(onGot, onError);
+            }
+
+        }, onError)
+        
     }
     /*
     Log the storage area that changed,
     then for each item changed,
     log its old value and its new value.
     */
+    var list_of_words = ''
     function logStorageChange(changes, area) {
         console.log("Change in storage area: " + area);
     
         var changedItems = Object.keys(changes);
-    
+        
         for (var item of changedItems) {
             console.log(item + " has changed:");
             console.log("Old value: ");
@@ -24,28 +34,56 @@
             console.log("New value: ");
             console.log(changes[item].newValue);
 
-            performMark(changes[item].newValue);
+            if(item === 'WORDS'){
+                //performMark(changes[item].newValue);
+                performMark(changes[item].newValue);
+                list_of_words = changes[item].newValue;
+            }
+
+            if(item === 'STATE' && changes[item].newValue === false){
+                //console.log('should be unmarked', changes[item].newValue)
+                markInstance.unmark()
+            }
+            if(item === 'STATE' && changes[item].newValue === true){
+                // console.log('should be MARKED', list_of_words);
+                browser.storage.local.get("WORDS")
+                    .then((res)=>{
+                        console.log(res["WORDS"], 'Shoud be marked');
+                        performMark(res["WORDS"]);
+                    }, onError)
+                // performMark(list_of_words);
+            }
         }
     }
     
+    
+
     browser.storage.onChanged.addListener(logStorageChange);
 
     function onGot(item) {
-        
-        console.log(item , 'we got that shit');
+      
+      performMark(item["WORDS"]);
 
     }
 
     function onError(error) {
-        console.log(`Error: ${error}`);
+      console.log(`Error: ${error}`);
     }
-
-
     
     function performMark(value) {
-        console.log('performMark', value)
+        //console.log('performMark', value) // xar,mar,marian
         // Remove previous marked elements and mark
         // the new keyword inside the context
-        markInstance.mark(value);
+        markInstance.unmark({
+            done: function(){
+              var partsOfStr = value.split(','); // se xer master
+
+              for(item in partsOfStr){
+                markInstance.mark(partsOfStr[item]);
+              }   
+          }
+        });
     };
+
+    
 })();
